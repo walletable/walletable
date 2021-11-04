f4e99d04a7c476fc37389c1fcd42371aee650731
Ilesanmi Olawale Adedotun <horlawaley001@gmail.com>
2021-11-05 00:52:08 +0100

    modified config file

--- config/walletable.php
+++ config/walletable.php
@@ -1,6 +1,5 @@
 <?php
 
-
 return [
 
     /*
@@ -10,33 +9,39 @@ return [
     |
     | Here you may specify which of the wallet drivers you wish to use
     | as your default driver for all wallet created incase you didn`t
-    | specify any driver during creation. Of course you may use many 
-    | drivers at once using the Database library.
+    | specify any driver during creation.
     |
     */
-
     'default' => env('WALLETABLE_DRIVER', 'database'),
 
-
+    /*
+    |--------------------------------------------------------------------------
+    | Model class names
+    |--------------------------------------------------------------------------
+    |
+    | You can set model class names here, so walletable and other
+    | related packages can use the correct class names
+    |
+    */
     'models' => [
-        'wallet' => Walletable\Models\Wallet::class,
-        'hold' => Walletable\Models\Hold::class,
-        'inbound' => Walletable\Models\Inbound::class,
-        'outbound' => Walletable\Models\Outbound::class,
-        'transaction' => Walletable\Models\Transaction::class,
+        'wallet' => \App\Models\Wallet::class,
+        'hold' => \App\Models\Hold::class,
+        'inbound' => \App\Models\Inbound::class,
+        'outbound' => \App\Models\Outbound::class,
+        'transaction' => \App\Models\Transaction::class,
     ],
 
-    'uiid' => [
-        'wallets' => false,
-        'holds' => false,
-        'inbounds' => false,
-        'outbounds' => false,
-        'transactions' => false
-    ],
-
-    'generation' => [
-        'tries' => 5,
-        'label' => 'Wallet',
-    ],
+    /*
+    |--------------------------------------------------------------------------
+    | Model uuid primary keys
+    |--------------------------------------------------------------------------
+    |
+    | By default, Walletable uses auto-incrementing primary keys when assigning
+    | IDs to clients. However, if Walletable is instaalling you will be asked
+    | to choose if you want to use uuid instead, this will be set to "true" and
+    | UUIDs will be used.
+    |
+    */
+    'model_uuids' => false,
 
 ];
