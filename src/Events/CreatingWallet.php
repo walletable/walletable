8562c1b71f1ea358475608eeddd574bd3437892f
Ilesanmi Olawale Adedotun <horlawaley001@gmail.com>
2021-11-16 11:53:56 +0100

    add servcie classes for creating a wallet

---
+++ src/Events/CreatingWallet.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Walletable\Events;
+
+use Illuminate\Queue\SerializesModels;
+
+class CreatingWallet
+{
+    use SerializesModels;
+
+    /**
+     * The created wallet driver.
+     *
+     * @var \Walletable\Drivers\DriverInterface
+     */
+    public $driver;
+
+    /**
+     * The created wallet.
+     *
+     * @var \Walletable\Models\Wallet
+     */
+    public $wallet;
+
+    /**
+     * The owner of the created wallet.
+     *
+     * @var \Walletable\Contracts\Walletable
+     */
+    public $walletable;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param  \Walletable\Drivers\DriverInterface  $driver
+     * @param  \Walletable\Models\Wallet  $wallet
+     * @param  \Walletable\Contracts\Walletable  $walletable
+     *
+     * @return void
+     */
+    public function __construct($driver, $wallet, $walletable)
+    {
+        $this->driver = $driver;
+        $this->wallet = $wallet;
+        $this->walletable = $walletable;
+    }
+}
