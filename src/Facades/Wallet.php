6edddce74523d5876b02ae8a0c74a17f4b29a754
Ilesanmi Olawale Adedotun <horlawaley001@gmail.com>
2022-07-16 23:56:49 +0100

    chore: add and correct doc-block of WalletManager

--- src/Facades/Wallet.php
+++ src/Facades/Wallet.php
@@ -6,7 +6,15 @@ use Illuminate\Support\Facades\Facade;
 use Walletable\WalletManager;
 
 /**
- *
+ * @method \Walletable\Models\Wallet create(\Walletable\Contracts\Walletable $walletable, string $label, string $tag, string $currency)
+ * @method bool compactible(Wallet $wallet, Wallet $against)
+ * @method mixed compactible(Wallet $wallet, Wallet $against)
+ * @method void macro($name, $macro)
+ * @method void flushMacros()
+ * @method bool hasMacro($name)
+ * @method void mixin($mixin, $replace = true)
+ * @method \Walletable\Internals\Lockers\LockerInterface|void locker(string $name, $locker = null)
+ * @method \Walletable\Internals\Actions\ActionInterface|void action(string $name, $action = null)
  */
 class Wallet extends Facade
 {
