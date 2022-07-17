4ecccf0414d69c33bd46086cc6603bc5b6585f6b
Ilesanmi Olawale Adedotun <horlawaley001@gmail.com>
2022-07-17 12:49:29 +0100

    chore: walletable facades doc-block include static methods

--- src/Facades/Wallet.php
+++ src/Facades/Wallet.php
@@ -6,15 +6,15 @@ use Illuminate\Support\Facades\Facade;
 use Walletable\WalletManager;
 
 /**
- * @method \Walletable\Models\Wallet create(\Walletable\Contracts\Walletable $walletable, string $label, string $tag, string $currency)
- * @method bool compactible(Wallet $wallet, Wallet $against)
- * @method mixed compactible(Wallet $wallet, Wallet $against)
- * @method void macro($name, $macro)
- * @method void flushMacros()
- * @method bool hasMacro($name)
- * @method void mixin($mixin, $replace = true)
- * @method \Walletable\Internals\Lockers\LockerInterface|void locker(string $name, $locker = null)
- * @method \Walletable\Internals\Actions\ActionInterface|void action(string $name, $action = null)
+ * @method static \Walletable\Models\Wallet create(\Walletable\Contracts\Walletable $walletable, string $label, string $tag, string $currency)
+ * @method static bool compactible(Wallet $wallet, Wallet $against)
+ * @method static mixed compactible(Wallet $wallet, Wallet $against)
+ * @method static void macro($name, $macro)
+ * @method static void flushMacros()
+ * @method static bool hasMacro($name)
+ * @method static void mixin($mixin, $replace = true)
+ * @method static \Walletable\Internals\Lockers\LockerInterface|void locker(string $name, $locker = null)
+ * @method static \Walletable\Internals\Actions\ActionInterface|void action(string $name, $action = null)
  */
 class Wallet extends Facade
 {
