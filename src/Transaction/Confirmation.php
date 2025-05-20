bd32470b3fd5582a77d9a259cbbc733f7edef336
Ilesanmi Olawale Adedotun <47121750+Mane-Olawale@users.noreply.github.com>
2025-05-20 11:23:12 +0100

    feat: unconfirmed and confirmation services Class

---
+++ src/Transaction/Confirmation.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Walletable\Transaction;
+
+use App\Models\Transaction;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Walletable\Exceptions\InsufficientBalanceException;
+use Walletable\Facades\Walletable;
+use Walletable\Internals\Lockers\LockerInterface;
+use Walletable\Models\Wallet;
+use Walletable\Money\Money;
+
+class Confirmation
+{
+    /**
+     * Transfer status
+     *
+     * @var bool
+     */
+    protected $successful = false;
+
+    /**
+     * Sender wallet
+     *
+     * @var \Walletable\Models\Wallet
+     */
+    protected $wallet;
+
+    /**
+     * Sender wallet
+     *
+     * @var \Walletable\Models\Transaction
+     */
+    protected $transaction;
+
+    /**
+     * The transfer locker
+     *
+     * @var \Walletable\Internals\Lockers\OptimisticLocker
+     */
+    protected $locker;
+
+    /**
+     * Execution Options
+     *
+     * @var array
+     */
+    protected $options;
+
+    public function __construct(
+        Wallet $wallet,
+        Transaction $transaction,
+        LockerInterface|null $locker = null,
+    ) {
+        $this->wallet = $wallet;
+        $this->transaction = $transaction;
+        $this->locker = $locker;
+    }
+
+    /**
+     * Execute the transfer
+     *
+     * @return self
+     */
+    public function execute(): self
+    {
+        $this->checks();
+        $locker = $this->locker();
+        $shouldInitiateTransaction = $locker->shouldInitiateTransaction($this->wallet, $this->transaction->amount, $this->transaction) ||
+            ($this->options['should_initiate_transaction'] ?? false);
+
+        try {
+            $method = $this->transaction->type . 'Lock';
+            $action = Walletable::action($this->transaction->getRawOriginal('action') ?? 'credit_debit');
+
+            if (!$action->{'support' . ucfirst($this->transaction->type) }()) {
+                throw new Exception('This action does not support ' . $this->transaction->type . ' operations', 1);
+            }
+
+            if ($shouldInitiateTransaction) {
+                DB::beginTransaction();
+            }
+
+            if ($this->locker()->$method($this->wallet, $this->transaction->amount, $this->transaction)) {
+                $this->successful = true;
+
+                $this->transaction->forceFill([
+                    'confirmed' => true,
+                    'confirmed_at' => now()
+                ])->save();
+            }
+
+            if ($shouldInitiateTransaction) {
+                DB::commit();
+            }
+        } catch (\Throwable $th) {
+            if ($shouldInitiateTransaction) {
+                DB::rollBack();
+            }
+            throw $th;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Run some compulsory checks
+     *
+     * @return void
+     */
+    protected function checks()
+    {
+        if (
+            $this->transaction->type === 'debit' &&
+            $this->wallet->amount->lessThan($this->transaction->amount)
+        ) {
+            throw new InsufficientBalanceException($this->wallet, $this->transaction->amount);
+        }
+    }
+
+    /**
+     * Get transaction bag
+     *
+     * @return \Walletable\Models\Transaction
+     */
+    public function transaction(): Transaction
+    {
+        return $this->transaction;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return \Walletable\Money\Money
+     */
+    public function getAmount(): Money
+    {
+        return $this->transaction->amount;
+    }
+
+    /**
+     * Get the locker for the transfer
+     */
+    protected function locker(): LockerInterface
+    {
+        if ($this->locker) {
+            return $this->locker;
+        }
+        return $this->locker = Walletable::locker(config('walletable.locker'));
+    }
+}
---
+++ src/Transaction/UnconfirmedCreditDebit.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Walletable\Transaction;
+
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+use Walletable\Internals\Actions\ActionInterface;
+use Walletable\Facades\Walletable;
+use Walletable\Internals\Actions\ActionData;
+use Walletable\Models\Transaction;
+use Walletable\Models\Wallet;
+use Walletable\Money\Money;
+
+class UnconfirmedCreditDebit
+{
+    /**
+     * Transaction type
+     *
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Sender wallet
+     *
+     * @var \Walletable\Models\Wallet
+     */
+    protected $wallet;
+
+    /**
+     * Amount to transfer
+     *
+     * @var \Walletable\Money\Money
+     */
+    protected $amount;
+
+    /**
+     * Trasanction bads
+     *
+     * @var \Walletable\Transaction\TransactionBag|\Illuminate\Support\Collection
+     */
+    protected $bag;
+
+    /**
+     * Title of the
+     *
+     * @var string|null
+     */
+    protected $title;
+
+    /**
+     * Note added to the transfer
+     *
+     * @var string|null
+     */
+    protected $remarks;
+
+    /**
+     * The session id of the transfer
+     *
+     * @var bool
+     */
+    protected $session;
+
+    /**
+     * Action of the transaction
+     *
+     * @var \Walletable\Internals\Actions\ActionInterface
+     */
+    protected $action;
+
+    /**
+     * Action of the transaction
+     *
+     * @var \Walletable\Internals\Actions\ActionData
+     */
+    protected $actionData;
+
+    public function __construct(
+        string $type,
+        Wallet $wallet,
+        Money $amount,
+        string|null $title = null,
+        string|null $remarks = null,
+    ) {
+        if (!in_array($type, ['credit', 'debit'])) {
+            throw new InvalidArgumentException('Argument 1 value can only be "credit" or "debit"');
+        }
+
+        $this->type = $type;
+        $this->wallet = $wallet;
+        $this->amount = $amount;
+        $this->title = $title;
+        $this->remarks = $remarks;
+        $this->session = Str::uuid();
+        $this->bag = new TransactionBag();
+    }
+
+    /**
+     * Execute the transfer
+     *
+     * @return self
+     */
+    public function execute(): self
+    {
+        $transaction = $this->bag->new($this->wallet, [
+            'type' => $this->type,
+            'session' => $this->session,
+            'remarks' => $this->remarks
+        ]);
+
+        $action = $this->action ?? Walletable::action('credit_debit');
+
+        Walletable::applyAction(
+            $action,
+            $this->bag,
+            $this->actionData ??  new ActionData(
+                $this->wallet,
+                $this->title
+            )
+        );
+
+        $this->bag->each(function ($item) {
+            $item->forceFill([
+                'confirmed' => false,
+                'created_at' => now()
+            ])->save();
+        });
+
+        return $this;
+    }
+
+    /**
+     * Get transaction
+     *
+     * @return \Walletable\Models\Transaction
+     */
+    public function transaction(): Transaction
+    {
+        return $this->bag->first();
+    }
+
+    /**
+     * Get transaction bag
+     *
+     * @return \Walletable\Transaction\TransactionBag
+     */
+    public function getTransactions(): TransactionBag
+    {
+        return $this->bag;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return \Walletable\Money\Money
+     */
+    public function getAmount(): Money
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set the action for the transaction
+     *
+     * @param \Walletable\Internals\Actions\ActionInterface|string $action
+     * @param \Walletable\Internals\Actions\ActionData $actionData
+     *
+     * @return self
+     */
+    public function setAction($action, ActionData $actionData): self
+    {
+        if (!is_string($action) && !($action instanceof ActionInterface)) {
+            throw new InvalidArgumentException(
+                sprintf('Argument 1 must be of type %s or String', ActionInterface::class)
+            );
+        }
+
+        if (is_string($action)) {
+            $action = Walletable::action($action);
+        }
+
+        $this->action = $action;
+        $this->actionData = $actionData;
+
+        return $this;
+    }
+}
